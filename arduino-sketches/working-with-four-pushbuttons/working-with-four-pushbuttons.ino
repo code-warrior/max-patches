b4a40a399e1cfde4ba55f4b80a47444a3b1e7758
Roy A Vanegas <roy@thecodeeducators.com>
2020-10-06 20:50:50 -0400

    Decrease indentation

--- arduino-sketches/working-with-four-pushbuttons/working-with-four-pushbuttons.ino
+++ arduino-sketches/working-with-four-pushbuttons/working-with-four-pushbuttons.ino
@@ -15,66 +15,66 @@ int previousStateOfButtonThree = 0;
 int previousStateOfButtonFour = 0;
 
 void setup() {
-   pinMode(BUTTON_1, INPUT);
-   pinMode(BUTTON_2, INPUT);
-   pinMode(BUTTON_3, INPUT);
-   pinMode(BUTTON_4, INPUT);
-   pinMode(LED, OUTPUT);
+  pinMode(BUTTON_1, INPUT);
+  pinMode(BUTTON_2, INPUT);
+  pinMode(BUTTON_3, INPUT);
+  pinMode(BUTTON_4, INPUT);
+  pinMode(LED, OUTPUT);
 
-   Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
-   stateOfButtonOne = digitalRead(BUTTON_1);
-   stateOfButtonTwo = digitalRead(BUTTON_2);
-   stateOfButtonThree = digitalRead(BUTTON_3);
-   stateOfButtonFour = digitalRead(BUTTON_4);
+  stateOfButtonOne = digitalRead(BUTTON_1);
+  stateOfButtonTwo = digitalRead(BUTTON_2);
+  stateOfButtonThree = digitalRead(BUTTON_3);
+  stateOfButtonFour = digitalRead(BUTTON_4);
 
-   if (stateOfButtonOne != previousStateOfButtonOne) {
-      if (stateOfButtonOne == HIGH) {
-         Serial.println("A1");
-         digitalWrite(LED, HIGH);
-      } else {
-         Serial.println("A0");
-         digitalWrite(LED, LOW);
-      }
+  if (stateOfButtonOne != previousStateOfButtonOne) {
+    if (stateOfButtonOne == HIGH) {
+      Serial.println("A1");
+        digitalWrite(LED, HIGH);
+    } else {
+      Serial.println("A0");
+      digitalWrite(LED, LOW);
+    }
 
-      previousStateOfButtonOne = stateOfButtonOne;
-   }
+    previousStateOfButtonOne = stateOfButtonOne;
+  }
 
-   if (stateOfButtonTwo != previousStateOfButtonTwo) {
-      if (stateOfButtonTwo == HIGH) {
-         Serial.println("B1");
-         digitalWrite(LED, HIGH);
-      } else {
-         Serial.println("B0");
-         digitalWrite(LED, LOW);
-      }
+  if (stateOfButtonTwo != previousStateOfButtonTwo) {
+    if (stateOfButtonTwo == HIGH) {
+      Serial.println("B1");
+      digitalWrite(LED, HIGH);
+    } else {
+      Serial.println("B0");
+      digitalWrite(LED, LOW);
+    }
 
-      previousStateOfButtonTwo = stateOfButtonTwo;
-   }
+    previousStateOfButtonTwo = stateOfButtonTwo;
+  }
 
-   if (stateOfButtonThree != previousStateOfButtonThree) {
-      if (stateOfButtonThree == HIGH) {
-         Serial.println("C1");
-         digitalWrite(LED, HIGH);
-      } else {
-         Serial.println("C0");
-         digitalWrite(LED, LOW);
-      }
+  if (stateOfButtonThree != previousStateOfButtonThree) {
+    if (stateOfButtonThree == HIGH) {
+      Serial.println("C1");
+      digitalWrite(LED, HIGH);
+    } else {
+      Serial.println("C0");
+      digitalWrite(LED, LOW);
+    }
 
-      previousStateOfButtonThree = stateOfButtonThree;
-   }
+    previousStateOfButtonThree = stateOfButtonThree;
+  }
 
-   if (stateOfButtonFour != previousStateOfButtonFour) {
-      if (stateOfButtonFour == HIGH) {
-         Serial.println("D1");
-         digitalWrite(LED, HIGH);
-      } else {
-         Serial.println("D0");
-         digitalWrite(LED, LOW);
-      }
+  if (stateOfButtonFour != previousStateOfButtonFour) {
+    if (stateOfButtonFour == HIGH) {
+      Serial.println("D1");
+      digitalWrite(LED, HIGH);
+    } else {
+      Serial.println("D0");
+      digitalWrite(LED, LOW);
+    }
 
-      previousStateOfButtonFour = stateOfButtonFour;
-   }
+    previousStateOfButtonFour = stateOfButtonFour;
+  }
 }
